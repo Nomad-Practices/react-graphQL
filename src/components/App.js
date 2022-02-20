0abcd5d7f439f9cb398c5f0818e49b503a461c1d
sjk <jkshin0903@gmail.com>
2022-02-20 20:36:54 +0900

    router 추가

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
-import { Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import Home from '../routes/Home.js'
 import Detail from '../routes/Detail.js'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/:id" element={<Detail />} />
-    </Routes>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:id" element={<Detail />} />
+      </Routes>
+    </HashRouter>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
-import { HashRouter } from 'react-router-dom'
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+
+const client = new ApolloClient({
+  uri: 'https://movieql.now.sh',
+  cache: new InMemoryCache(),
+})
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter>
+    <ApolloProvider client={client}>
       <App />
-    </HashRouter>
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
