aabb971d7fee77ca4b473768c79858bd60f639e0
sjk <jkshin0903@gmail.com>
2022-02-20 20:02:30 +0900

    react router settings + reset.css + prittier config

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,14 @@
+import { Routes, Route } from 'react-router-dom'
+import Home from '../routes/Home.js'
+import Detail from '../routes/Detail.js'
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/:id" element={<Detail />} />
+    </Routes>
+  )
 }
 
-export default App;
+export default App
