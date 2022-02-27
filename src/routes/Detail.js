88c5646b3a345457f7fc608e456677a22006de99
sjk <jkshin0903@gmail.com>
2022-02-27 11:05:13 +0900

    Home, Detail 화면 제작
    apollo caching, fragment

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,6 +1,44 @@
 import { useParams } from 'react-router-dom'
 import { gql, useQuery } from '@apollo/client'
-import { Loading } from '../components/styled'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  height: 100vh;
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: white;
+`
+
+const Column = styled.div`
+  margin-left: 10px;
+  width: 50%;
+`
+
+const Title = styled.h1`
+  font-size: 65px;
+  margin-bottom: 15px;
+`
+
+const Subtitle = styled.h4`
+  font-size: 35px;
+  margin-bottom: 10px;
+`
+
+const Description = styled.p`
+  font-size: 20px;
+`
+
+const Poster = styled.div`
+  width: 25%;
+  height: 60%;
+  background-color: transparent;
+  background-size: cover;
+  background-position: center center;
+  background-image: url(${(props) => props.bg});
+`
 
 // query에 parameter를 전달할 때는 query의 이름을 명시해야 한다
 // parameter 변수는 무조건 $ prefix와 data type을 붙여야 한다.
@@ -10,9 +48,10 @@ import { Loading } from '../components/styled'
 const GET_MOVIE = gql`
   query getMovie($id: Int!) {
     movie(id: $id) {
-      id
       title
       medium_cover_image
+      language
+      rating
       description_intro
     }
   }
@@ -21,16 +60,33 @@ const GET_MOVIE = gql`
 function Detail() {
   const { id } = useParams()
   // useQuery에서 parameter 전달은 variables option으로 넘겨준다.
+
+  // ApooloClient는 한번 사용한 query에 대한 응답값을 caching하기 때문에 gql server로 다시 요청을 보내지 않는다.
+  // 그래서 api loading을 줄일 수 있다 -> 더 빠르게 contents를 제공할 수 있다.
   const { loading, data } = useQuery(GET_MOVIE, {
     variables: { id: +id },
   })
 
-  if (loading) {
-    return <Loading>Loading...</Loading>
-  }
-  if (data && data.movie) {
-    return <h1>{data.movie.title}</h1>
-  }
+  return (
+    <Container>
+      <Column>
+        <Title>{loading ? 'Loading...' : data.movie.title}</Title>
+        {!loading && data.movie && (
+          // 여러 개의 컴포넌트들을 굳이 html element로 wrapping하지 않을거면 fragment(<></>)를 사용하면 된다
+          // Vue에서 컴포넌트 template 내부에서 <template></template>과 동일한 기능을 한다.
+          <>
+            <Subtitle>
+              {data.movie.language} · {data.movie.rating}
+            </Subtitle>
+            <Description>{data.movie.description_intro}</Description>
+          </>
+        )}
+      </Column>
+      {data && data.movie && (
+        <Poster bg={data.movie.medium_cover_image}></Poster>
+      )}
+    </Container>
+  )
 }
 
 export default Detail
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,13 +1,50 @@
 import { gql, useQuery } from '@apollo/client'
-import {
-  Container,
-  Header,
-  Title,
-  Subtitle,
-  Loading,
-  Movies,
-} from '../components/styled'
 import Movie from '../components/Movie'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`
+
+const Header = styled.header`
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  height: 45vh;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`
+
+const Title = styled.h1`
+  font-size: 60px;
+  font-weight: 600;
+  margin-bottom: 20px;
+`
+
+const Subtitle = styled.h3`
+  font-size: 35px;
+`
+
+const Loading = styled.div`
+  font-size: 18px;
+  opacity: 0.5;
+  font-weight: 500;
+  margin-top: 10px;
+`
+
+const Movies = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 25px;
+  width: 60%;
+  position: relative;
+  top: -50px;
+`
 
 const GET_MOVIES = gql`
   query {
@@ -28,7 +65,13 @@ function Home() {
         <Subtitle>GraphQL amazing</Subtitle>
       </Header>
       {loading && <Loading>Loading</Loading>}
-      {data && data.movies.map((m) => <Movie key={m.id} id={m.id} />)}
+      {data && data.movies && (
+        <Movies>
+          {data.movies.map((m) => (
+            <Movie key={m.id} id={m.id} bg={m.medium_cover_image} />
+          ))}
+        </Movies>
+      )}
     </Container>
   )
 }
